9cefc02ff9d8c5a4812513242dabd850ee9b3c05
Balraj Singh <thisisbalraj17@gmail.com>
2024-01-03 18:39:40 +0530

    Refactored code for improved readability and brevity

--- index.js
+++ index.js
@@ -6,48 +6,7 @@ const randomGenres = [
   "Life-Facts",
 ];
 
-// full lists
-const motivation = [
-  "Believe in yourself; you're capable of more than you imagine.",
-  "Success often comes to those who dare to dream, who are not afraid to take risks.",
-  "Your attitude determines your direction; stay positive, work hard, and success will follow.",
-  "Every accomplishment starts with the decision to try; take that first step toward your goal.",
-  "Embrace challenges as opportunities for growth; perseverance is the key to achieving greatness.",
-];
-
-const lifeFacts = [
-  "Change is Inevitable",
-  "Learning is Continuous",
-  "Challenges Nurture Growth",
-  "Connections Matter",
-  "Time is Precious",
-];
-
-const comedyHumorList = [
-  "Why did the comedian go to school? To improve his 'stand-up' performance!",
-  "I'm on a seafood diet. I see food and I eat it... except during a comedy show; then I try not to choke!",
-  "Why don't comedians do knock-knock jokes? Because they don't want to 'stand-up' for the answer!",
-  "Did you hear about the claustrophobic comedian? He couldn't 'stand' the 'close' set!",
-  "How many comedians does it take to screw in a light bulb? One, but the light bulb has to 'twist' the punchline!",
-];
-
-const scienceList = [
-  "Science is the art of asking questions and seeking answers through experimentation.",
-  "The scientific method: observe, question, hypothesize, test, analyze, conclude, and share.",
-  "The beauty of science lies in its ability to unveil the mysteries of the universe.",
-  "In science, every failure is a step closer to understanding. It's about progress, not perfection.",
-  "Science is a journey of discovery, exploring the unknown and expanding our understanding.",
-];
-
-const mysteryList = [
-  "Mystery novels often keep readers on the edge of their seats, unraveling enigmatic plots.",
-  "The intrigue of a mystery lies in the anticipation of discovering the unknown.",
-  "The allure of a mystery is in its ability to captivate, leaving audiences curious and engaged.",
-  "Solving a mystery requires critical thinking, attention to detail, and piecing together clues.",
-  "The thrill of a mystery is the satisfaction of uncovering the concealed truths.",
-];
-
-// advices
+// our advices
 const AdviceList = [
   "Listen more than you speak; understanding often begins with attentive listening.",
   "Don't fear failure; it's a stepping stone toward success, offering valuable lessons.",
@@ -56,59 +15,74 @@ const AdviceList = [
   "Surround yourself with positivity; choose company that uplifts and supports you.",
 ];
 
-// function random
+// all msgs genre with their msgs
+const msgObject = {
+  motivation: [
+    "Believe in yourself; you're capable of more than you imagine.",
+    "Success often comes to those who dare to dream, who are not afraid to take risks.",
+    "Your attitude determines your direction; stay positive, work hard, and success will follow.",
+    "Every accomplishment starts with the decision to try; take that first step toward your goal.",
+    "Embrace challenges as opportunities for growth; perseverance is the key to achieving greatness.",
+  ],
 
-const makeRandomNumber = (array) => {
-  return Math.floor(Math.random() * array.length);
+  "life-Facts": [
+    "Change is Inevitable",
+    "Learning is Continuous",
+    "Challenges Nurture Growth",
+    "Connections Matter",
+    "Time is Precious",
+  ],
+
+  mystery: [
+    "Mystery novels often keep readers on the edge of their seats, unraveling enigmatic plots.",
+    "The intrigue of a mystery lies in the anticipation of discovering the unknown.",
+    "The allure of a mystery is in its ability to captivate, leaving audiences curious and engaged.",
+    "Solving a mystery requires critical thinking, attention to detail, and piecing together clues.",
+    "The thrill of a mystery is the satisfaction of uncovering the concealed truths.",
+  ],
+
+  science: [
+    "Science is the art of asking questions and seeking answers through experimentation.",
+    "The scientific method: observe, question, hypothesize, test, analyze, conclude, and share.",
+    "The beauty of science lies in its ability to unveil the mysteries of the universe.",
+    "In science, every failure is a step closer to understanding. It's about progress, not perfection.",
+    "Science is a journey of discovery, exploring the unknown and expanding our understanding.",
+  ],
+
+  comedy: [
+    "Why did the comedian go to school? To improve his 'stand-up' performance!",
+    "I'm on a seafood diet. I see food and I eat it... except during a comedy show; then I try not to choke!",
+    "Why don't comedians do knock-knock jokes? Because they don't want to 'stand-up' for the answer!",
+    "Did you hear about the claustrophobic comedian? He couldn't 'stand' the 'close' set!",
+    "How many comedians does it take to screw in a light bulb? One, but the light bulb has to 'twist' the punchline!",
+  ],
 };
 
-// choosing random genre
-const genreChoiceFunk = (choiceArray) => {
-  return choiceArray[makeRandomNumber(choiceArray)];
-};
+const randomChoice =
+  randomGenres[Math.floor(Math.random() * randomGenres.length)];
 
-// choosing random msg according to random genre choice
-const randomMsgGenrator = (genre) => {
-  let randomMessage = "";
-  if (genre === "Motivation") {
-    randomMessage = motivation[makeRandomNumber(motivation)];
-  } else if (genre === "Mystery") {
-    randomMessage = mysteryList[makeRandomNumber(mysteryList)];
-  } else if (genre === "Science") {
-    randomMessage = scienceList[makeRandomNumber(scienceList)];
-  } else if (genre === "Comedy") {
-    randomMessage = comedyHumorList[makeRandomNumber(comedyHumorList)];
-  } else if (genre === "Life-Facts") {
-    randomMessage = lifeFacts[makeRandomNumber(lifeFacts)];
-  } else {
-    randomMessage = "Unknown genre or issue with genreChoiceFunk function.";
+//console.log(randomChoice);
+
+// function
+
+const messageGeneratorFunction = (choice, obj, advice) => {
+  let choiceToday = choice;
+  let msgToday = "";
+  let randomAdvice = advice[Math.floor(Math.random() * advice.length)];
+
+  for (let key in obj) {
+    if (choice.toLowerCase() === key.toLocaleLowerCase()) {
+      msgToday = obj[key][Math.floor(Math.random() * obj[key].length)];
+    }
   }
-  return randomMessage;
+
+  return `\n Random Genre Choice is: ${choiceToday} \n \n Message:👇 \n ${msgToday} \n \n Advice From Us: ☺ \n ${randomAdvice}`;
 };
 
-// free advice
+let finalMessage = messageGeneratorFunction(
+  randomChoice,
+  msgObject,
+  AdviceList
+);
 
-const adviceFunk = (array) => {
-  return array[makeRandomNumber(array)];
-};
-
-//saved data
-// genreting random genre
-const saveRandomGenre = genreChoiceFunk(randomGenres);
-
-// genreting random msg according that genre choice
-const saveRandomMsg = randomMsgGenrator(saveRandomGenre);
-
-// life advice
-const saveRandomAdvice = adviceFunk(AdviceList);
-
-// msg app
-const messageApp = (Genre, msg, advice) => {
-  console.log(`Topic of your Message is: ${Genre}\n`);
-  console.log(`Message: \n ${msg} \n `);
-  console.log(`An Advice: ${advice} \n`);
-};
-
-messageApp(saveRandomGenre, saveRandomMsg, saveRandomAdvice);
-
-// I will update this, make it more shorter using objects.
+console.log(finalMessage);
